db56fcc7c523016a44020bc5a846da987a7ebf87
AMalfez <a_mansuri@ch.iitr.ac.in>
2024-11-08 17:21:30 +0530

    tried fixing hero text

--- components/customer/Hero.tsx
+++ components/customer/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = ()=>{
         <section className="w-full h-fit flex flex-col-reverse justify-center items-center pb-5 md:pb-0 md:w-10/12 md:h-4/6 m-auto md:mt-7 md:rounded-3xl bg-orange-300 lg:flex-row">
             <div className="w-4/6 sm:pb-8 lg:w-1/2 flex flex-col lg:pl-20 lg:pr-5 justify-center">
                 <p className="text-center text-2xl lg:text-left md:text-4xl font-bold"><span className="text-rose-800">Pre-book</span> your favorate restaurants</p>
-                <p className="text-center hidden md:block lg:text-left text-2xl text-white mt-3 pr-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum nulla ratione soluta consequatur quia adipisci exercitationem enim, tempore iure accusantium.</p>
+                <p className="text-center text-2xl hidden md:block lg:text-left text-white mt-3 pr-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum nulla ratione soluta consequatur quia adipisci exercitationem enim, tempore iure accusantium.</p>
             </div>
             <div className="w-1/2 flex justify-center items-center">
                 <Image
